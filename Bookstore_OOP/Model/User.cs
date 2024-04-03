e844df6adb0031c0da676d3b390071d9fbe54cae
Karina Timoshevich <karina03tima@gmail.com>
2024-04-03 09:23:14 +0300

    add user class

---
+++ Bookstore_OOP/Model/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace Bookstore_OOP.Model
+{
+     public class User
+     {
+        private int _id;
+        private string _email;
+        private string _password;
+        private string _Name;
+        private string _phoneNumber;
+        private string _address;
+
+        // [PrimaryKey, AutoIncrement]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get => _id; set => _id = value; }
+        public string Email { get => _email; set => _email = value; }
+        public string Password { get => _password; set => _password = value; }
+        public string Name { get => _Name; set => _Name = value; }
+        public string PhoneNumber { get => _phoneNumber; set => _phoneNumber = value; }
+        public string Address { get => _address; set => _address = value; }
+    }
+}
